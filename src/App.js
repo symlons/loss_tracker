548642021ae49357fcf0abb2bf1d1edfb990e6b6
symlons <fabio.kost@gmail.com>
2021-04-06 14:45:05 +0200

    echarts configuration

--- src/App.js
+++ src/App.js
@@ -1,25 +1,84 @@
-import logo from './logo.svg';
 import './App.css';
+import ReactECharts from 'echarts-for-react';
+import React from 'react';
+import { io } from "socket.io-client";
+const socket = io('http://localhost:3005');
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends React.Component {
+
+  componentDidMount() {
+
+    const myChart = this.echartRef.getEchartsInstance();
+    let data = []
+    let count = []
+    let dataX = [];
+    let delay = 1000;
+
+    socket.on('yCoordinates', (socket) => {
+      //console.log(socket.yCoordinates)
+      dataX.push(parseInt(socket.xCoordinates));
+      data.push({ name: socket.xCoordinates, value: [socket.xCoordinates, socket.yCoordinates] });
+
+
+
+      count++;
+    })
+    setInterval(function () {
+
+      myChart.setOption({
+        xAxis: {
+          type: 'value',
+          //min:dataX[0],
+          max: dataX[dataX.length - 1] + 1,
+        },
+        series: [{
+          data: data,
+          animationThreshold: 5000
+        }]
+      });
+
+    }, delay);
+
+  }
+
+  getOption() {
+
+    let option = {
+      dataZoom: [
+        {
+          type: 'slider',
+          filterMode: 'none'
+        },],
+      xAxis: {
+        type: 'value',
+      },
+      yAxis: {
+        type: 'value',
+      },
+      series: [{
+        showSymbol: false,
+        hoverAnimation: false,
+        type: 'line',
+        animation: true,
+
+      }]
+    };
+
+    return option;
+  }
+
+  render() {
+
+    return (
+      <div
+        style={{
+          width: "60%",
+          height: "100%",
+        }}>
+        <ReactECharts ref={(e) => { this.echartRef = e; }} option={this.getOption()} opts={{ renderer: 'svg' }} />
+      </div>
+    )
+  }
 }
 
 export default App;
