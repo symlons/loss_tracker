70dba6e0669e98a104d983cf70ddc222ed27109f
Fabio Kost <kostfab1@students.zhaw.ch>
2024-10-18 12:39:04 +0200

    fix dev routing

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -5,11 +5,11 @@ export default defineConfig({
   plugins: [react()],
   server: {
     proxy: {
-      "/api": {
+      "/api": { // only for development
         target: "http://localhost:5005",
         changeOrigin: true,
         secure: false,
-        //rewrite: (path) => path.replace(/^\/api/, ""),
+        rewrite: (path) => path.replace(/^\/api/, ""),
       },
     },
   },
