e7d1df5723751015a076e717f802cefc372e0e95
Fabio Kost <kostfab1@students.zhaw.ch>
2025-03-05 22:40:27 +0100

    tailwind fixes

--- client/vite.config.ts
+++ client/vite.config.ts
@@ -1,12 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
+import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig(({ mode }) => {
   const isProduction = mode === 'production';
   console.log(isProduction)
   
   return {
-    plugins: [react()],
+    plugins: [react(),  tailwindcss()],
     server: {
       proxy: {
         "/api": {
